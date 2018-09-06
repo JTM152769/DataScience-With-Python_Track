8a4c52d8841f05915c22630dc6b7613581437f83
Amit Sah <32677659+JTM152769@users.noreply.github.com>
2018-09-06 08:07:34 +0000

    Update a-little-bit-of-twitter-text-analysis.py

--- 06-importing-data-in-python-(part-2)/3-diving-deep-into-the-twitter-api/a-little-bit-of-twitter-text-analysis.py
+++ 06-importing-data-in-python-(part-2)/3-diving-deep-into-the-twitter-api/a-little-bit-of-twitter-text-analysis.py
@@ -1,3 +1,25 @@
+'''A little bit of Twitter text analysis
+
+Now that you have your DataFrame of tweets set up, you're going to do a bit of text analysis to count how many tweets 
+contain the words 'clinton', 'trump', 'sanders' and 'cruz'. In the pre-exercise code, we have defined the following 
+function word_in_text(), which will tell you whether the first argument (a word) occurs within the 2nd argument (a tweet).
+
+import re
+
+def word_in_text(word, text):
+    word = word.lower()
+    text = tweet.lower()
+    match = re.search(word, text)
+
+    if match:
+        return True
+    return False
+
+You're going to iterate over the rows of the DataFrame and calculate how many tweets contain each of our keywords! 
+The list of objects for each candidate has been initialized to 0.'''
+
+
+
 import re
 
 def word_in_text(word, tweet):
