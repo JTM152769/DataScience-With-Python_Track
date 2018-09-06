ead3b343a0c9cb7589ba4caf3385acc6c7d1bd5a
Amit Sah <32677659+JTM152769@users.noreply.github.com>
2018-09-06 06:23:26 +0000

    Create pop-quiz.py

---
+++ 06-importing-data-in-python-(part-2)/2-interacting-with-apis-to-import-data-from-the-web/pop-quiz.py
@@ -0,0 +1,14 @@
+Pop quiz: What exactly is a JSON?
+
+Possible Answers
+
+    JSONs consist of key-value pairs.
+  
+    JSONs are human-readable.
+  
+    The JSON file format arose out of a growing need for real-time server-to-browser communication.
+   
+    ##Answer
+    ##The function json.load() will load the JSON into Python as a list.
+   
+    The function json.load() will load the JSON into Python as a dictionary.
