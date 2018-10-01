833b3cd760d6a074c60211a3d1a0e59371a69eae
Amit Sah <32677659+JTM152769@users.noreply.github.com>
2018-10-01 07:52:50 +0000

    Update case-study.py

--- 13-introduction-to-databases-in-python/case-study.py
+++ 13-introduction-to-databases-in-python/case-study.py
@@ -3,3 +3,22 @@ From connecting to a database, to populating it, to reading and querying it, you
 key concepts you learned in this course. 
 
 Enjoy!'''
+
+
+'''Setup the Engine and MetaData
+
+In this exercise, your job is to create an engine to the database that will be used in this chapter. 
+Then, you need to initialize its metadata.
+Recall how you did this in Chapter 1 by leveraging create_engine() and MetaData.'''
+
+# Import create_engine, MetaData
+from sqlalchemy import MetaData, create_engine
+
+# Define an engine to connect to chapter5.sqlite: engine
+engine = create_engine('sqlite:///chapter5.sqlite')
+
+# Initialize MetaData: metadata
+metadata = MetaData()
+
+
+
