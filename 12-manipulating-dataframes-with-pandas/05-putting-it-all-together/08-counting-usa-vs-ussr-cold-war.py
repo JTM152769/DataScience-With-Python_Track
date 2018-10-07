5d6616b496d19c9a6b19b1f691503186ee6babb4
Amit Sah <32677659+JTM152769@users.noreply.github.com>
2018-10-07 23:13:41 +0530

    Update 08-counting-usa-vs-ussr-cold-war.py

--- 12-manipulating-dataframes-with-pandas/05-putting-it-all-together/08-counting-usa-vs-ussr-cold-war.py
+++ 12-manipulating-dataframes-with-pandas/05-putting-it-all-together/08-counting-usa-vs-ussr-cold-war.py
@@ -28,3 +28,19 @@ most_medals = cold_war_usa_usr_medals.idxmax(axis='columns')
 
 # Print most_medals.value_counts()
 print(most_medals.value_counts())
+
+==============================================================================================
+
+
+# Create the pivot table: medals_won_by_country
+medals_won_by_country = medals.pivot_table(index='Edition', columns='NOC', values='Athlete', aggfunc='count')
+
+# Slice medals_won_by_country: cold_war_usa_urs_medals
+cold_war_usa_urs_medals = medals_won_by_country.loc[1952:1988, ['USA','URS']]
+
+# Create most_medals
+most_medals = cold_war_usa_urs_medals.idxmax(axis='columns')
+
+# Print most_medals.value_counts()
+print(most_medals.value_counts())
+
